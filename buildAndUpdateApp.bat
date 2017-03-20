03ba53cb803893565936a3279cfb539add846855
Ceyhun ÖZGÜN <ceyhun_ozgun@hotmail.com>
2017-03-20 11:18:43 +0300

    Update buildAndUpdateApp.bat
    Removed comment

--- buildAndUpdateApp.bat
+++ buildAndUpdateApp.bat
@@ -1,3 +1,3 @@
 call mvn package
 call aws s3 cp target/cardstore-0.0.1-SNAPSHOT.war s3://cardstoredeploy/
-rem java -jar awsupdateasg-0.0.1-SNAPSHOT.jar CardStoreASG
+java -jar awsupdateasg-0.0.1-SNAPSHOT.jar CardStoreASG
